3eb48a14feda0f17b01eb9c5032da73636cf7ffc
Matthew Hornsey <hornseym@live.co.uk>
2022-01-23 19:25:54 +0000

    Added for tests for what an item should do (i.e. have a name and price).

--- test/test_item.py
+++ test/test_item.py
@@ -4,4 +4,18 @@ from context import item
 
 def test_item_initialisable():
     """Test that an item can be initialise without exception."""
-    assert item.Item()
+    assert item.Item("A", 50)
+
+
+def test_that_item_initialise_with_correct_name():
+    """When items are initialised they are given a name; this name should 
+    be correctly stored and returned."""
+    sku = item.Item("A", 50)
+    assert sku.name == "A"
+
+
+def test_item_initialise_with_correct_name():
+    """When items are initialised they are given a price; this price should 
+    be correctly stored and returned."""
+    sku = item.Item("A", 50)
+    assert sku.price == 50
