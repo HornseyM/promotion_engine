7682ad32f272c07a2c3bce5b1599f744aa0c1e45
Matthew Hornsey <hornseym@live.co.uk>
2022-01-23 19:33:27 +0000

    Added properties and missing variables. Tests pass.

--- src/item.py
+++ src/item.py
@@ -1,4 +1,20 @@
 """Module to contain the item class."""
 
 class Item:
-    """An item"""
+    """An item, suitable for adding to a basket.
+    
+    :param name: The name of the item.
+    :param price: The price of the item.
+    """
+
+    def __init__(self, name: str, price: int) -> None:
+        self._name: str = name
+        self._price: int = price
+
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    @property
+    def price(self) -> int:
+        return self._price
