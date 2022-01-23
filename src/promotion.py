549d8f50b35aa2c4683520d880705ba6e9e8865d
Matthew Hornsey <hornseym@live.co.uk>
2022-01-23 20:58:49 +0000

    Added class for promotion on A.

---
+++ src/promotion.py
@@ -0,0 +1,5 @@
+"""Module to store the curent promotions (for now)."""
+
+
+class ProAs:
+    """The promotion on A is buy 3 for 130. This class will find them in a basket and apply the promotion."""
