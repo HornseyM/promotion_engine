3c82c26b22c70b3bc0b70322fe6d10d1bf3a32e3
Matthew Hornsey <hornseym@live.co.uk>
2022-01-23 19:50:08 +0000

    Added tests for building all the items.

--- test/test_item_builder.py
+++ test/test_item_builder.py
@@ -4,3 +4,59 @@ from context import item
 def test_item_builder_initialise_without_exception():
     """Test that an item builder can be initialise without throwing an exception."""
     assert item.ItemBuilder()
+
+
+def test_build_item_a_with_name():
+    """Item Builder should be able to build item A with correct name."""
+    builder = item.ItemBuilder
+    sku = builder.get_a()
+    assert sku.name == 'A'
+
+
+def test_build_item_a_with_price():
+    """Item Builder should be able to build item A with correct price."""
+    builder = item.ItemBuilder
+    sku = builder.get_a()
+    assert sku.price == 50
+
+
+def test_build_item_b_with_name():
+    """Item Builder should be able to build item B with correct name."""
+    builder = item.ItemBuilder
+    sku = builder.get_b()
+    assert sku.name == 'B'
+
+
+def test_build_item_b_with_price():
+    """Item Builder should be able to build item B with correct price."""
+    builder = item.ItemBuilder
+    sku = builder.get_b()
+    assert sku.price == 30
+
+
+def test_build_item_c_with_name():
+    """Item Builder should be able to build item C with correct name."""
+    builder = item.ItemBuilder
+    sku = builder.get_c()
+    assert sku.name == 'C'
+
+
+def test_build_item_c_with_price():
+    """Item Builder should be able to build item C with correct price."""
+    builder = item.ItemBuilder
+    sku = builder.get_c()
+    assert sku.price == 20
+
+
+def test_build_item_d_with_name():
+    """Item Builder should be able to build item D with correct name."""
+    builder = item.ItemBuilder
+    sku = builder.get_d()
+    assert sku.name == 'D'
+
+
+def test_build_item_d_with_price():
+    """Item Builder should be able to build item D with correct price."""
+    builder = item.ItemBuilder
+    sku = builder.get_D()
+    assert sku.price == 15
