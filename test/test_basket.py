78fefc95aab204b833956a7c892366d41e88f99b
Matthew Hornsey <hornseym@live.co.uk>
2022-01-23 20:15:47 +0000

    Added tests for returning and summing items in an empty basket.

--- test/test_basket.py
+++ test/test_basket.py
@@ -4,3 +4,20 @@ from context import basket
 def test_basket_initialisable_without_exception():
     """Should be able to initalise a simple (e.g. empty) basket without an exception."""
     assert basket.Basket()
+
+
+def test_initialising_basket_with_no_items():
+    """Should be able to provide no items to a basket and it still initialises."""
+    assert basket.Basket([])
+
+
+def test_empty_basket_should_return_no_items():
+    """If a basket is empty and the items are request it should return no items."""
+    bas = basket.Basket()
+    assert not bas.items
+
+
+def test_empty_basket_should_have_zero_total():
+    """If a basket is empty then it's total should be 0."""
+    bas = basket.Basket()
+    assert bas.total == 0
