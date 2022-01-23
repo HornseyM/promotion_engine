e69c083d2c8339304723abc59f965d83372e028e
Matthew Hornsey <hornseym@live.co.uk>
2022-01-23 22:10:33 +0000

    Added test for scenario B. It passes.

--- test/test_acceptence_tests.py
+++ test/test_acceptence_tests.py
@@ -1,6 +1,6 @@
 import pytest
 
-from context import item, basket
+from context import item, basket, promotion
 
 """Aceptence test from customer are as follows:
 
@@ -49,18 +49,33 @@ def test_scenario_a(builder):
     Scenario A
     (1 * A) + (1 * B) + (1 * C) = 100
     """
+    # Make the basket of items
     items = [builder.get_a(), builder.get_b(), builder.get_c()]
     bas = basket.Basket(items)
+    # Check the total is calculated correctly
     assert bas.total == 100
 
 
-def test_scenario_b():
+def test_scenario_b(builder):
     """Scenario B is defined as follows:
     
     Scenario B
     (5 * A) + (5 * B) + (1 * C) = 370
     """
-    assert False  # ToDo Complete when code exists
+    # Get the promotions
+    pro_a = promotion.ProAs()
+    pro_b = promotion.ProBs()
+    # Get the items and make the basket
+    all_a = [builder.get_a() for ii in range(5)]
+    all_b = [builder.get_b() for ii in range(5)]
+    items = all_a + all_b + [builder.get_c()]
+    bas = basket.Basket(items)
+    # Apply promotion A once
+    bas = pro_a.apply(bas)
+    # Apply promotion B once
+    bas = pro_b.apply(pro_b.apply(bas))
+    # Check the total is calculated with promotions applied
+    assert bas.total == 370
 
 
 def test_scenario_c():
