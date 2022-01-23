87917d306b0a910303a93f129b7a17775d549a1d
Matthew Hornsey <hornseym@live.co.uk>
2022-01-23 20:22:45 +0000

    Added methods in Basket class to pass tests for empty basket.

--- src/basket.py
+++ src/basket.py
@@ -1,5 +1,25 @@
+from typing import List, Optional
+
+from .item import Item
+
 """Module to contain the Basket class and any helpers."""
 
 
 class Basket:
-    """A basket to hold and output items of a shop."""
+    """A basket to hold and output items of a shop.
+    
+    :param items: The items to store in the basket.
+    """
+
+    def __init__(self, items: Optional[List[Item]] = None) -> None:
+        self._items: List[Item] = items or []
+
+    @property
+    def items(self) -> List[Item]:
+        """Return the items in the basket."""
+        return self._items
+
+    @property
+    def total(self) -> int:
+        """The total of the items in the basket."""
+        return 0
