19504fe05474cc7593708b82347cfa2cecdc8780
Matthew Hornsey <hornseym@live.co.uk>
2022-01-23 20:33:23 +0000

    Added tests for more items in a basket.

--- test/test_basket.py
+++ test/test_basket.py
@@ -1,4 +1,12 @@
-from context import basket
+from sre_parse import fix_flags
+import pytest
+
+from context import basket, item
+
+
+@pytest.fixture()
+def builder():
+    return item.ItemBuilder()
 
 
 def test_basket_initialisable_without_exception():
@@ -21,3 +29,41 @@ def test_empty_basket_should_have_zero_total():
     """If a basket is empty then it's total should be 0."""
     bas = basket.Basket()
     assert bas.total == 0
+
+
+def test_basket_with_single_item_in_initialises(builder):
+    """Initialise a basket with a single item in without exception."""
+    assert basket.Basket([builder.get_a()])
+
+
+def test_basket_with_multiple_items_in_initialises(builder):
+    """Initialise a basket with a multiple items in without exception."""
+    assert basket.Basket([builder.get_a(), builder.get_b(), builder.get_c()])
+
+
+def test_basket_with_single_item_returns_item(builder):
+    """A basket with a single item in should be able to return it."""
+    items = [builder.get_a()]
+    bas = basket.Basket(items)
+    assert bas.items == items
+
+
+def test_basket_with_multiple_items_returns_the_items(builder):
+    """A basket with multiple items in should return them all."""
+    items = [builder.get_a(), builder.get_b(), builder.get_c()]
+    bas = basket.Basket(items)
+    assert bas.items == items
+
+
+def test_basket_with_single_item_calculates_total(builder):
+    """A basket with a single item in should calculate the correct total."""
+    items = [builder.get_a()]
+    bas = basket.Basket(items)
+    assert bas.total == 50
+
+
+def test_basket_with_multiple_items_returns_the_items(builder):
+    """A basket with multiple items in should calculate the correct total."""
+    items = [builder.get_a(), builder.get_b(), builder.get_c()]
+    bas = basket.Basket(items)
+    assert bas.total == 100
